d4ef5e962c8d9ce328609bbd6b01b23e9178a87e
Metehan Çavdar <metehan262626@gmail.com>
2021-03-01 12:38:08 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ClassMetotDemo
+{
+    class CustomerManager
+    {
+        
+            List<Customer> customers = new List<Customer>();
+
+            public void Add(Customer customer)
+            {
+                customers.Add(customer);
+                Console.WriteLine(customer.FirstName + " " + customer.LastName + " " + "Added");
+            }
+
+            public void Delete(Customer customer)
+            {
+                customers.Remove(customer);
+                Console.WriteLine(customer.FirstName + " " + customer.LastName + " " + "Deleted");
+            }
+
+
+            public void List()
+            {
+                foreach (var customer in customers)
+                {
+                    Console.WriteLine(customer.Id + " " + customer.FirstName + " " + customer.LastName + " " + customer.EMail);
+                }
+            }
+        }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace ClassMetotDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            customer1.Id = 1;
+            customer1.FirstName ="Mete Han ";
+            customer1.LastName = "Çavdar";            
+            customer1.EMail = "metehan262626@gmail.com";
+
+            Customer customer2 = new Customer();
+            customer2.Id = 2;
+            customer2.FirstName = "Meto";
+            customer2.LastName = "Meto";            
+            customer2.EMail = "metometo@gmail.com";
+
+            Customer customer3 = new Customer();
+            customer3.Id = 3;
+            customer3.FirstName = "Engin";
+            customer3.LastName = "Demiroğ";
+            customer3.EMail = "Engindemirog@gmail.com";
+
+
+            CustomerManager customerManager = new CustomerManager();
+
+            customerManager.Add(customer1);
+            customerManager.Add(customer2);
+            customerManager.Add(customer3);
+            customerManager.List();
+
+
+           
+
+        }
+    }
+}
